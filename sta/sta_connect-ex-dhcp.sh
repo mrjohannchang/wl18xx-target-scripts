f6c66162e75c6a31fa7142530534e4df8f1a7bed
Yaniv Machani <yanivma@ti.com>
2014-10-09 18:10:55 +0300

    sta: fix dhcpc scripts for Station
     - small fix- check only if DHCPC was started for wlan.
    
    Signed-off-by: Yaniv Machani <yanivma@ti.com>

--- sta/sta_connect-ex-dhcp.sh
+++ sta/sta_connect-ex-dhcp.sh
@@ -2,7 +2,7 @@
 
 sleep 2
 ### start udhcp client, if not started
-output=`ps | grep -v grep | grep udhcpc`
+output=`ps | grep -v grep | gerp "udhcpc -i wlan0"`
 set -- $output
 echo $output
 if [ -z "$output" ]; then
